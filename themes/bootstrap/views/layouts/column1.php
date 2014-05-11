d70e59c2a54e2bfd81642b9c615ed7cde503a42d
Jurets <jurets75@gmail.com>
2014-05-11 23:49:21 +0300

    * подрихтовать разделы

--- themes/bootstrap/views/layouts/column1.php
+++ themes/bootstrap/views/layouts/column1.php
@@ -4,42 +4,24 @@
 
 <?php
 $allStatuses =  Orders::model()->StatusCount();
-//print_r($s);
 $items = array();
+$isStatus = isset($_GET['status']);
 $items[0] = array(
 	'label'=>"Новые заказы",
 	'url'=>Yii::app()->createUrl('site/index'),
+    'active'=>!$isStatus,
 );
-foreach ($allStatuses as $key=>$value)
-{
+foreach ($allStatuses as $key=>$value) {
 	$items[$key]=array(
 		'label'=>CHtml::encode($value),
 		'url'=>Yii::app()->createUrl('site/index',array('status'=>$key)),
-		//'active'=>true
+		'active'=>$isStatus && ($_GET['status'] == $key),
 	);
 }
-
-/*
-foreach ($s as $key=>$value)
-{
-	echo CHtml::link(
-		CHtml::encode($value), 
-		Yii::app()->createUrl(
-			'site/index',
-			array('status'=>$key)
-		)
-	);	
-}*/ 
 
 $this->widget('bootstrap.widgets.TbMenu', array(
     'type'=>'pills', // '', 'tabs', 'pills' (or 'list')
     'stacked'=>false, // whether this is a stacked menu
-    /*
-    'items'=>array(
-        array('label'=>'Home', 'url'=>'#', 'active'=>true),
-        array('label'=>'Profile', 'url'=>'#'),
-        array('label'=>'Messages', 'url'=>'#'),
-    */
     'items'=>$items
 )); 
 ?>
